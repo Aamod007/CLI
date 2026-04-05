83c960daeb4fec370eab9a3fc1335441bd79131b
Aamod007 <aamodkumar2006@gmail.com>
2026-04-05 14:33:41 +0530

    Server migration

--- server/src/lib/db.js
+++ server/src/lib/db.js
@@ -1,7 +1,16 @@
+import "dotenv/config";
 import {PrismaClient} from '@prisma/client';
+import {PrismaPg} from '@prisma/adapter-pg';
 
 const globalForPrisma = global
-const prisma = new PrismaClient();
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+    throw new Error('DATABASE_URL is not set');
+}
+
+const adapter = new PrismaPg({connectionString});
+const prisma = globalForPrisma.prisma ?? new PrismaClient({adapter});
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma; 
 
